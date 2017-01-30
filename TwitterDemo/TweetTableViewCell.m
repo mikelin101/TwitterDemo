d2e430e5d5074e17bd4758ceb8aa971e9d928e78
Michael Lin <mili@yahoo-inc.com>
2017-01-30 14:00:51 -0800

    end of lecture state

--- TwitterDemo/TweetTableViewCell.m
+++ TwitterDemo/TweetTableViewCell.m
@@ -23,9 +23,10 @@
 - (void)awakeFromNib {
     [super awakeFromNib];
     self.nameLabel.text = @"My Twitter Name";
-    self.handleLabel.text = @"@somebody";
+    self.handleLabel.text = @"@somebodysomebodysomebodysomebodysomebodysomebodysomebodysomebodysomebodysomebodysomebody";
     self.timestampLabel.text = @"4h";
     self.contentLabel.text = @"A really long repeated message. A really long repeated message. A really long repeated message. A really long repeated message.";
+//    self.retweetContainerHeightConstraint.constant = 0;
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
